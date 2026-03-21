39963619128b64060770aae667fb25206b303865
shine7on <pabboo13@gmail.com>
2026-03-20 22:19:04 -0500

    Add serach/filter functions in admin page

--- shelter_web/admin.py
+++ shelter_web/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Dog
 
 # Register your models here.
-admin.site.register(Dog)
+# admin.site.register(Dog)
+
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'breed', 'sex', 'status')
+    search_fields = ('name', 'breed')
+    # list_filter = ('status')
