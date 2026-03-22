c69fd9fd39ca77b243310dd4e0a4db40633d8153
shine7on <pabboo13@gmail.com>
2026-03-21 20:12:41 -0500

    Add Breed model so that admin has drop down for dog breeds

--- shelter_web/models.py
+++ shelter_web/models.py
@@ -1,23 +1,29 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+# from admin_searchable_dropdown import AutocompleteFilter
 import requests
 
 # Create your models here.
-class Dog(models.Model):
-    SexType = models.TextChoices("SexType", "Female Male")
-    StatusType = models.TextChoices("StatusType", 'Adopted Pending Not-Adopted')
-
-    name = models.CharField(max_length=100)
-    breed = models.CharField(max_length=100)
-    age = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(35)])
-    weight = models.FloatField(default=0, validators=[MinValueValidator(0)])
-    sex = models.CharField(choices=SexType)
-    status = models.CharField(choices=StatusType)
-    description = models.CharField(max_length=200)
-
+'''
+class BreedFilter(AutocompleteFilter):
+    title = 'Breed'
+    field_name = 'breed'
+'''
 
 class Breed(models.Model):
     breed = models.CharField(max_length=100)
 
     def __str__(self):
         return self.breed
+    
+class Dog(models.Model):
+    SexType = models.TextChoices("SexType", "Female Male")
+    StatusType = models.TextChoices("StatusType", 'Adopted Pending Not-Adopted')
+
+    name = models.CharField(max_length=100)
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE)
+    age = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(35)])
+    weight = models.FloatField(default=0, validators=[MinValueValidator(0)])
+    sex = models.CharField(choices=SexType)
+    status = models.CharField(choices=StatusType)
+    description = models.CharField(max_length=200)
