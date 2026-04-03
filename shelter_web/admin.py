b08ba946c264eee8cf96d6325b024c7504d2141e
shine7on <pabboo13@gmail.com>
2026-04-02 21:23:37 -0500

    Fix the displayed name of weight

--- shelter_web/admin.py
+++ shelter_web/admin.py
@@ -11,9 +11,12 @@ class DogAdmin(admin.ModelAdmin):
     def age_display(self, obj):
         return f'{obj.age_year}y {obj.age_month}m'
     
+    def weight_display(self, obj):
+        return f'{obj.weight} kg'
+    
     # change the name of variable to display
     age_display.short_description = 'age'
-    age_display.short_description = 'weight[kg]'
+    weight_display.short_description = 'weight[kg]'
 
     list_display = ('name', 'age_display', 'breed', 'weight', 'sex', 'status')
     search_fields = ('name', 'status')
