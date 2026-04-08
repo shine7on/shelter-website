cef842f74a706582243f4b5b9bf2075c89a5d84f
shine7on <pabboo13@gmail.com>
2026-04-08 10:36:19 -0500

    Add path to dog detail page

--- shelter_web/urls.py
+++ shelter_web/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('hello', views.hello_world, name='hello_world'),
     path('dog', views.all_dogs_views, name='dogs'),
-    path('dogapi', views.dog_list_api, name='dogos_api'),
+    path('dogapi', views.dog_list_api, name='dogs_api'),
+    path('dog/<int:dog_id>/', views.dog_detail, name='dog_detail')
 ]
